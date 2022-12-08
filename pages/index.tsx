8097c5e18c1de69ea4cdfbf6b6a1b9660c9079af
Tomoaki Sakurai <ssaku.power@gmail.com>
2022-12-08 18:37:06 +0900

    add: homeに移動

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,83 +1,166 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import { Button, Typography } from '@material-tailwind/react';
+import { Layout } from 'components/layouts';
+import { useRouter } from 'next/router';
+import { ReactElement, useEffect, useMemo, useState } from 'react';
+import {
+  getTimeHHmmss,
+  getTimeDMS,
+  getTimeHHmm,
+  getDiffHHmm,
+  getMillToTimeHHmmss,
+} from 'utils/time';
 
-const Home: NextPage = () => {
+type RestTime = {
+  start: Date | null;
+  end: Date | null;
+  duration: number;
+};
+
+export default function Home() {
+  const [time, setTime] = useState(new Date());
+  const [startTime, setStartTime] = useState<Date | null>(null);
+  const [endTime, setEndTime] = useState<Date | null>(null);
+  const [restTimes, setRestTimes] = useState<RestTime[]>([]);
+  const router = useRouter();
+
+  const handleBeginClick = () => {
+    if (endTime !== null) {
+      // 休憩開始
+      const current = new Date();
+      const duration = current.getTime() - endTime.getTime();
+      setRestTimes([...restTimes, { start: endTime, end: current, duration: duration }]);
+      setEndTime(null);
+      return;
+    }
+
+    setStartTime(time);
+  };
+
+  const handleEndClick = () => {
+    setEndTime(time);
+  };
+
+  const message = '編集内容がリセットされます、本当にページ遷移しますか？';
+
+  const pageChangeHandler = () => {
+    const answer = window.confirm(message);
+    if (!answer) {
+      throw 'routeChange aborted.';
+    }
+  };
+
+  useEffect(() => {
+    if (startTime !== null) {
+      router.events.on('routeChangeStart', pageChangeHandler);
+      window.addEventListener('beforeunload', handleBeforeUnload);
+      return () => {
+        router.events.off('routeChangeStart', pageChangeHandler);
+        window.removeEventListener('beforeunload', handleBeforeUnload);
+      };
+    }
+  }, [startTime]);
+
+  const handleBeforeUnload = (e: BeforeUnloadEvent) => {
+    e.preventDefault();
+    e.returnValue = '編集内容がリセットされます、本当にページ遷移しますか？';
+  };
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timerId);
+  }, [time]);
+
+  const durationSum = useMemo(() => {
+    return restTimes.reduce((acc, cur) => {
+      return acc + cur.duration;
+    }, 0);
+  }, [restTimes]);
+
+  const workingTimeSum = useMemo(() => {
+    if (startTime === null || endTime === null) return '00:00:00';
+    return getDiffHHmm(endTime, startTime);
+    // return Math.round((endTime.getTime() - startTime.getTime()) / 1000);
+  }, [endTime]);
+
+  const isDisabledStartButton = !!(startTime !== null && endTime === null);
+  const isDisabledEndButton = !!(startTime === null || (startTime !== null && endTime !== null));
+
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center py-2'>
-      <Head>
-        <title>Create Next App</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
+    <div className='mt-20'>
+      <Typography className='mb-8 text-3xl'>{time.toLocaleDateString()}</Typography>
+      <Typography className='mb-8 text-3xl text-6xl' variant='h1'>
+        {getTimeHHmmss(time)}
+      </Typography>
+      <Button
+        disabled={isDisabledStartButton}
+        className='mr-3'
+        variant={!isDisabledStartButton ? 'filled' : 'outlined'} //'filled'
+        color={!isDisabledStartButton ? 'blue' : 'gray'}
+        size='lg'
+        onClick={handleBeginClick}
+      >
+        出勤
+      </Button>
+      <Button
+        disabled={isDisabledEndButton}
+        variant={!isDisabledEndButton ? 'filled' : 'outlined'}
+        size='lg'
+        color={!isDisabledEndButton ? 'blue' : 'gray'}
+        onClick={handleEndClick}
+      >
+        退勤
+      </Button>
 
-      <main className='flex w-full flex-1 flex-col items-center justify-center px-20 text-center'>
-        <h1 className='text-6xl font-bold'>
-          Welcome to{' '}
-          <a className='text-blue-600' href='https://nextjs.org'>
-            Next.js!
-          </a>
-        </h1>
-
-        <p className='mt-3 text-2xl'>
-          Get started by editing{' '}
-          <code className='rounded-md bg-gray-100 p-3 font-mono text-lg'>pages/index.tsx</code>
-        </p>
-
-        <div className='mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full'>
-          <a
-            href='https://nextjs.org/docs'
-            className='mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600'
-          >
-            <h3 className='text-2xl font-bold'>Documentation &rarr;</h3>
-            <p className='mt-4 text-xl'>
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href='https://nextjs.org/learn'
-            className='mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600'
-          >
-            <h3 className='text-2xl font-bold'>Learn &rarr;</h3>
-            <p className='mt-4 text-xl'>
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href='https://github.com/vercel/next.js/tree/canary/examples'
-            className='mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600'
-          >
-            <h3 className='text-2xl font-bold'>Examples &rarr;</h3>
-            <p className='mt-4 text-xl'>
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href='https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-            className='mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600'
-          >
-            <h3 className='text-2xl font-bold'>Deploy &rarr;</h3>
-            <p className='mt-4 text-xl'>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className='flex justify-center pt-8 pb-10'>
+        <div className='px-8'>
+          <Typography className='block' variant='h4'>
+            出勤時刻
+          </Typography>
+          <Typography className='block' variant='h4'>
+            {startTime !== null ? `${getTimeHHmmss(startTime)}` : ''}
+          </Typography>
         </div>
-      </main>
+        <div className='px-8'>
+          <Typography className='block' variant='h4'>
+            退勤時刻
+          </Typography>
+          <Typography className='block' variant='h4'>
+            {endTime !== null ? `${getTimeHHmmss(endTime)}` : ''}
+          </Typography>
+        </div>
+        <div className='px-8'>
+          <Typography className='block' variant='h4'>
+            勤務時間
+          </Typography>
+          <Typography className='block' variant='h4'>
+            {workingTimeSum}
+          </Typography>
+        </div>
+      </div>
+      {restTimes.length > 0 && (
+        <Typography className='block' variant='paragraph'>
+          合計休憩時間: {getMillToTimeHHmmss(durationSum)}
+        </Typography>
+      )}
 
-      <footer className='flex h-24 w-full items-center justify-center border-t'>
-        <a
-          className='flex items-center justify-center gap-2'
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Powered by <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
-        </a>
-      </footer>
+      {/* {restTimes.map((time) => {
+        return (
+          <div className='pb-4'>
+            <Typography variant='paragraph'>
+              休憩開始: {time.start?.toLocaleTimeString()}
+            </Typography>
+            <Typography variant='paragraph'>休憩終了: {time.end?.toLocaleTimeString()}</Typography>
+            <Typography variant='paragraph'>休憩時間: {Math.round(time.duration)}秒</Typography>
+          </div>
+        );
+      })} */}
     </div>
-  )
+  );
 }
 
-export default Home
+Home.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>;
+};
