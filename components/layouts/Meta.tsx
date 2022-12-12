4676f039141ad299496fb8652400a39c93ff9fb3
Tomoaki Sakurai <ssaku.power@gmail.com>
2022-12-12 20:01:42 +0900

    add: titleを追加

--- components/layouts/Meta.tsx
+++ components/layouts/Meta.tsx
@@ -3,7 +3,9 @@ import Head from 'next/head';
 export const Meta = () => {
   return (
     <Head>
+      <title>勤怠くん</title>
       <meta name='robots' content='noindex' />
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
     </Head>
   );
 };
