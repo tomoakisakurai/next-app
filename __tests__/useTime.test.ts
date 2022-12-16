4f48484043a1b7b87a8aeb67ade697e216e50144
Tomoaki Sakurai <ssaku.power@gmail.com>
2022-12-17 00:43:05 +0900

    UT

--- __tests__/useTime.test.ts
+++ __tests__/useTime.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react';
 import { useTime } from 'hooks/useTime';
 
 describe('useTime', () => {
