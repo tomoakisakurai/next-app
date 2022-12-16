36d2eede205fba9f560f8ecf107af9b0124ebcda
Tomoaki Sakurai <ssaku.power@gmail.com>
2022-12-17 01:34:15 +0900

    切り戻し

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './vitest.setup.js',
+    // setupFiles: './vitest.setup.js',
   },
 });
--- vitest.setup.js
+++
@@ -1,9 +0,0 @@
-const ignoredErrors = [/act\(\.\.\.\) is not supported in production builds of React/];
-
-const consoleError = global.console.error;
-global.console.error = (...args) => {
-  if (ignoredErrors.some((el) => el.test(args[0]))) {
-    return console.log(...args);
-  }
-  return consoleError(...args);
-};
