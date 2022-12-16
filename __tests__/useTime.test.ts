c46ffcc7f1bbead742954143de63c41170b48213
Tomoaki Sakurai <ssaku.power@gmail.com>
2022-12-17 00:58:12 +0900

    UT

--- __tests__/useTime.test.ts
+++ __tests__/useTime.test.ts
@@ -1,11 +1,18 @@
 import { describe, it, expect } from 'vitest';
-import { renderHook } from '@testing-library/react';
+import { renderHook, act } from '@testing-library/react';
 import { useTime } from 'hooks/useTime';
 
 describe('useTime', () => {
   //   it.todo('useTime');
   it('time', () => {
     const { result } = renderHook(() => useTime());
-    expect(result.current.startTime).toBe(null);
+
+    const mockDate = new Date(2000, 1, 1, 12, 50);
+
+    act(() => {
+      result.current.setStartTime(mockDate);
+    });
+
+    expect(result.current.startTime).toBe(mockDate);
   });
 });
