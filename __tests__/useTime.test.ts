2f583802ff086a04153c15d3a3dda21a0e2a0d9a
Tomoaki Sakurai <ssaku.power@gmail.com>
2022-12-17 00:33:49 +0900

    UT

--- __tests__/useTime.test.ts
+++ __tests__/useTime.test.ts
@@ -3,9 +3,9 @@ import { renderHook } from '@testing-library/react-hooks';
 import { useTime } from 'hooks/useTime';
 
 describe('useTime', () => {
-  it.todo('useTime');
-  //   it('time', () => {
-  //     const { result } = renderHook(() => useTime());
-  //     expect(result.current.startTime).toBe(null);
-  //   });
+  //   it.todo('useTime');
+  it('time', () => {
+    const { result } = renderHook(() => useTime());
+    expect(result.current.startTime).toBe(null);
+  });
 });
